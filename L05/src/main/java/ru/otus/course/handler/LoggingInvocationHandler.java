0c85c6656e5ff3a856009a132e06724437bb0a93
stepninkv <stepninkv@it.mos.ru>
2024-08-11 13:14:15 +0300

    L05

--- L05/src/main/java/ru/otus/course/handler/LoggingInvocationHandler.java
+++ L05/src/main/java/ru/otus/course/handler/LoggingInvocationHandler.java
@@ -1,25 +1,37 @@
 package ru.otus.course.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.course.annotation.Log;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class LoggingInvocationHandler implements InvocationHandler {
 
   private static final Logger log = LoggerFactory.getLogger(LoggingInvocationHandler.class);
 
   private final Object target;
+  private final Set<Method> cache;
 
   public LoggingInvocationHandler(Object target) {
     this.target = target;
+    this.cache = Arrays.stream(target.getClass().getMethods())
+      .filter(method -> method.isAnnotationPresent(Log.class))
+      .collect(Collectors.toSet());
+  }
+
+  private boolean isMethodLoggable(Method method) {
+    return cache.stream()
+      .anyMatch(m -> m.getName().equals(method.getName()) && Arrays.equals(m.getParameterTypes(), method.getParameterTypes()));
   }
 
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    if (method.isAnnotationPresent(Log.class)) {
+    if (isMethodLoggable(method)) {
       StringBuilder logMessage = new StringBuilder();
       logMessage.append("executed method: ").append(method.getName());
 
