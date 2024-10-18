322632636b1c77a36b2c3c90f19929336b35b826
stepninkv <stepninkv@it.mos.ru>
2024-10-18 10:35:56 +0300

    L12

---
+++ L12/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+  implementation ("org.projectlombok:lombok")
+  annotationProcessor ("org.projectlombok:lombok")
+
+  implementation("ch.qos.logback:logback-classic")
+}
---
+++ L12/src/main/java/ru/otus/course/Main.java
@@ -0,0 +1,9 @@
+package ru.otus.course;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+
+}
